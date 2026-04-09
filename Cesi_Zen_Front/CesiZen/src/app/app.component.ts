b0cdaa7df1e110790342b58dea26e544a8331123
Rayzokynn <alexis.ledantec@viacesi.fr>
2026-04-09 11:45:18 +0200

    [fix] fix menu et home page

--- Cesi_Zen_Front/CesiZen/src/app/app.component.ts
+++ Cesi_Zen_Front/CesiZen/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NavigationEnd, Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { IonApp, IonRouterOutlet, IonSplitPane, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonMenuToggle, IonItem, IonIcon, IonLabel, IonFooter,} from '@ionic/angular/standalone';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,13 @@ import { IonApp, IonRouterOutlet, IonSplitPane, IonMenu, IonHeader, IonToolbar, 
   imports: [ IonApp, IonRouterOutlet, IonSplitPane, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonMenuToggle, IonItem, IonIcon, IonLabel, RouterLink, RouterLinkActive, IonFooter,],
 })
 export class AppComponent {
-  constructor() {
+  public isMenuDisabled = false;
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter((event: any) => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      const hiddenPages = ['/connexion']; 
+      this.isMenuDisabled = hiddenPages.includes(event.urlAfterRedirects);
+    });
   }
 }
